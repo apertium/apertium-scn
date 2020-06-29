75ff8c9fde77e8f0fed5db208f2d129436e45453
Flammie Pirinen <tommi.pirinen@uit.fi>
2020-06-29 21:17:09 +0200

    sfixed bracketing

--- dev/extract-cdiffs.bash
+++ dev/extract-cdiffs.bash
@@ -2,4 +2,4 @@ cut -f 1 variation/scn-ipa.wikt-seq2seq | sed -e 's/\(.\)/\1 /g' > foo
 cut -f 3 variation/scn-ipa.wikt-seq2seq | sed -e 's/\(.\)/\1 /g' > bar
 iconv -f utf-8 -t latin1 foo > fool
 iconv -f utf-8 -t latin1 bar > barl
-wdiff fool barl | iconv -f latin1 -t utf-8 | sed -e 's/-] {+/ -> /g' -e 's/-]/ -> 0/g' -e 's/{+/0 -> /g' -e 's/\[-/[ /g' -e 's/+}/ ]/g' > variation/repls.regex
+wdiff -n fool barl | iconv -f latin1 -t utf-8 | sed -e 's/-] {+/ -> /g' -e 's/-]/ -> 0 ]/g' -e 's/{+/[ 0 -> /g' -e 's/\[-/[ /g' -e 's/+}/ ]/g' > variation/repls.regex
