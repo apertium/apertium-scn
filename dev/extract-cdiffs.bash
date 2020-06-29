ab007fe42ce953b69078a108099ea9b320e36dba
Flammie Pirinen <tommi.pirinen@uit.fi>
2020-06-29 20:41:43 +0200

    bash script to do cdiffs

---
+++ dev/extract-cdiffs.bash
@@ -0,0 +1,5 @@
+cut -f 1 variation/scn-ipa.wikt-seq2seq | sed -e 's/\(.\)/\1 /g' > foo
+cut -f 3 variation/scn-ipa.wikt-seq2seq | sed -e 's/\(.\)/\1 /g' > bar
+iconv -f utf-8 -t latin1 foo > fool
+iconv -f utf-8 -t latin1 bar > barl
+wdiff fool barl | iconv -f latin1 -t utf-8 | sed -e 's/-] {+/ -> /g' -e 's/-]/ -> 0/g' -e 's/{+/0 -> /g' -e 's/\[-/[ /g' -e 's/+}/ ]/g' > variation/repls.regex
